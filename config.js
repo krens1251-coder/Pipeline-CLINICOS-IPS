12d165e84072f2c00ea805dc8de0aa27e06f2a7b
krens1251-coder <krens1251@gmail.com>
2026-04-15 16:37:19 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.PIPELINE_CONFIG = {
-  supabaseUrl: "https://sybmdxvyjppogqxwohsq.supabase.co",
-  supabaseAnonKey: "sb_publishable_aYQmMLYW4dZYHhOYmM-IdA_KK3tnmSh",
-  clientsTable: "pipeline_clients",
-  stateTable: "pipeline_shared_state"
-};
