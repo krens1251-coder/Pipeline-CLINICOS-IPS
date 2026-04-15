394fc9bc4457745bbf94a8ab67287e5753179d48
krens1251-coder <krens1251@gmail.com>
2026-04-15 16:38:40 -0500

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,6 @@
+window.PIPELINE_CONFIG = {
+  supabaseUrl: "https://TU_PROJECT_URL.supabase.co",
+  supabaseAnonKey: "TU_SUPABASE_PUBLISHABLE_KEY",
+  clientsTable: "pipeline_clients",
+  stateTable: "pipeline_shared_state"
+};
