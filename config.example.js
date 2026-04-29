89afac64c2bff070dbe9a281e524c6b025d61873
krens1251-coder <krens1251@gmail.com>
2026-04-29 14:59:25 -0500

    Delete config.example.js

--- config.example.js
+++
@@ -1,6 +0,0 @@
-window.PIPELINE_CONFIG = {
-  supabaseUrl: "https://TU_PROJECT_URL.supabase.co",
-  supabaseAnonKey: "TU_SUPABASE_PUBLISHABLE_KEY",
-  clientsTable: "pipeline_clients",
-  stateTable: "pipeline_shared_state"
-};
