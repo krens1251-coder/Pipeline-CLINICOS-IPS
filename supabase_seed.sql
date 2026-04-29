fe30369aab7a157db14e736ada74b8f803f04b1d
krens1251-coder <krens1251@gmail.com>
2026-04-29 15:18:12 -0500

    Delete supabase_seed.sql

--- supabase_seed.sql
+++
@@ -1,4 +0,0 @@
--- Opcional: crea el registro inicial de la herramienta.
-insert into public.app_state (key, data)
-values ('clinicos_pipeline_rebuilt_v3', '{}'::jsonb)
-on conflict (key) do nothing;
