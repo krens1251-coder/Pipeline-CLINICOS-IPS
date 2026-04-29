ed51233fa50a8e232668d98afc1a5a24b39898e6
krens1251-coder <krens1251@gmail.com>
2026-04-29 15:54:09 -0500

    Delete supabase_seed.sql

--- supabase_seed.sql
+++
@@ -1,4 +0,0 @@
--- Crea la fila compartida inicial. Ejecutar después de supabase_schema.sql.
-insert into public.app_state (id, data, updated_at)
-values ('pipeline', '{}'::jsonb, now())
-on conflict (id) do nothing;
